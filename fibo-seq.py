f1322e3e3358cbdc0ad1c5ed087c87354497fb28
sein010402 <sein010402@gmail.com>
2025-11-12 12:15:08 +0900

    feat: Add binet

--- fibo-seq.py
+++ fibo-seq.py
@@ -1,11 +1,14 @@
-def fibo(n):
-	pad = {0: 0, 1: 1}
-	def fib_inner(n):
-		if n not in pad:
-			pad[n] = fib_inner(n - 1) + fib_inner(n - 2)
-		return pad[n]
-	return fib_inner(n)
+import math
 
+def binet_fibonacci(n):
+  """
+  Binet의 공식을 사용하여 n번째 피보나치 수를 계산합니다.
+  """
+  phi = (1 + math.sqrt(5)) / 2
+  # psi = (1 - math.sqrt(5)) / 2 # psi 항은 매우 작아져서, n이 클 경우 0으로 수렴합니다.
+  # 따라서 아래와 같이 간단하게 계산할 수 있습니다.
+  result = (phi**n) / math.sqrt(5)
+  return round(result)
 
-if __name__ == '__main__':
-    print(fibo(4))
+# 예시
+print(binet_fibonacci(10))
