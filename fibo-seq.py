5eb4d18e496f92193202da9fa1af82c719b7a828
sein010402 <sein010402@gmail.com>
2025-11-12 12:07:05 +0900

    feat: Add memoization

--- fibo-seq.py
+++ fibo-seq.py
@@ -1,5 +1,11 @@
 def fibo(n):
-    if n < 2:
-        return n
-    else:
-        fibo(n - 1) + fibo(n - 2)
+	pad = {0: 0, 1: 1}
+	def fib_inner(n):
+		if n not in pad:
+			pad[n] = fib_inner(n - 1) + fib_inner(n - 2)
+		return pad[n]
+	return fib_inner(n)
+
+
+if __name__ == '__main__':
+    print(fibo(4))
